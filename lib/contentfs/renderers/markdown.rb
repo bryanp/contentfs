e28393ecb28671b79d8ce9f2bd5f4a9a0b4e18e6
Bryan Powell <bryan@metabahn.com>
2021-04-07 14:11:26 -0700

    Add support for typed blockquotes

--- lib/contentfs/renderers/markdown.rb
+++ lib/contentfs/renderers/markdown.rb
@@ -8,7 +8,37 @@ module ContentFS
     class Markdown
       class << self
         def render(content)
-          CommonMarker.render_html(content, [:DEFAULT, :UNSAFE])
+          renderer.render(CommonMarker.render_doc(content))
+        end
+
+        private def renderer
+          ContentFSRenderer.new(options: [:DEFAULT, :UNSAFE])
+        end
+      end
+
+      class ContentFSRenderer < CommonMarker::HtmlRenderer
+        def blockquote(node)
+          blockquote_type = if (match = node.to_plaintext.strip.match(/\[(.*)\]/))
+            match[1]
+          end
+
+          blockquote_class = if blockquote_type
+            " class=\"#{blockquote_type}\""
+          end
+
+          block do
+            container("<blockquote#{sourcepos(node)}#{blockquote_class}>\n", "</blockquote>") do
+              node.each.with_index do |child, index|
+                content = if blockquote_class && index == 0
+                  child.to_html.gsub("<p>[#{blockquote_type}] ", "<p>")
+                else
+                  child
+                end
+
+                out(content)
+              end
+            end
+          end
         end
       end
     end
